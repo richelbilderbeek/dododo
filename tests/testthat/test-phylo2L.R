47e537db7b429b568bcded87cebfa8cb3fac93ff
richelbilderbeek <richel@richelbilderbeek.nl>
2019-01-09 15:58:13 +0100

    EOD

--- tests/testthat/test-phylo2L.R
+++ tests/testthat/test-phylo2L.R
@@ -7,3 +7,15 @@ test_that("phylogeny to L table and back results in the original phylogeny", {
   phylogeny_again <- DDD::L2phylo(l_table)
   expect_equal(phylogeny, phylogeny_again)
 })
+
+test_that("L table is a data frame that has column names", {
+
+  skip("")
+  phylogeny <- ape::read.tree(text = "((t1:1, t3:1):1, t2:2);")
+  l_table <- phylo2L(phylogeny)
+  expect_true(is.data.frame(l_table))
+  col_names <- colnames(l_table)
+  expected <- c("birth_time", "parent", "id", "death_time")
+  expect_equal(col_names, expected)
+})
+
